d7fc2c5cad6f82b6e5b949e2cf29f0fc0bb16e5d
eko24ive <eko24ive@gmail.com>
2017-03-01 17:00:57 +0200

    simple weather api

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 var express = require('express')
 var app = express()
 app.set('port', (process.env.PORT || 5000));
+var fetch = require('node-fetch');
+ 
+// if you are on node v0.10, set a Promise library first, eg. 
+// fetch.Promise = require('bluebird'); 
+ 
+// plain text or html 
+ 
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+	var city = req.query.city
+	fetch('http://api.apixu.com/v1/current.json?key='+ process.env.WEATHER_API +'&q=' + city)
+	.then(function(res) {
+		return res.json();
+	}).then(function(body) {
+		res.send(body)
+	});
+	
 })
 
 app.listen(app.get('port'), function() {
